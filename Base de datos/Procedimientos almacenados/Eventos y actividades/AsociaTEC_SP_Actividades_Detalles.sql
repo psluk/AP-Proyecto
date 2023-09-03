c7c2452df41f66b6f6970f0ae82e48421a66c315
fabiansj0028 <fabiansj0028@gmail.com>
2023-09-03 00:36:35 -0600

    SP's Lista Actividades - Detalle Actividad

---
+++ Base de datos/Procedimientos almacenados/Eventos y actividades/AsociaTEC_SP_Actividades_Detalles.sql
@@ -0,0 +1,79 @@
+--------------------------------------------------------------------------
+-- Autor:       Fabián Vargas
+-- Fecha:       23-09-03
+-- Descripción: Retorna los detalles de un evento especifico
+--------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE [dbo].[AsociaTEC_SP_Actividades_Detalles]
+    @IN_uuid UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON;         -- No retorna metadatos
+
+    -- CONTROL DE ERRORES
+    DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @Message VARCHAR(200);
+    DECLARE @transaccionIniciada BIT = 0;
+
+    -- DECLARACIÓN DE VARIABLES
+    -- 
+
+    BEGIN TRY
+
+        -- VALIDACIONES
+        
+        IF NOT EXISTS(
+            SELECT 1
+            FROM [dbo].[Actividades] A
+            WHERE A.[uuid] = @IN_uuid
+            AND A.[eliminado] = 0
+        )
+        BEGIN
+            RAISERROR('No existe la actividad', 50000, 1)
+        END
+
+        SELECT COALESCE(
+            (
+                SELECT A.[lugar],
+                       A.[fechaInicio],
+                       A.[fechaFin]
+                FROM [dbo].[Actividades]
+                WHERE A.[uuid] = @IN_uuid
+                AND A.[eliminado] = 0
+                FOR JSON PATH
+            ), '[]'
+        ) as 'resultados'
+
+    END TRY
+    BEGIN CATCH
+
+        SET @ErrorNumber = ERROR_NUMBER();
+        SET @ErrorSeverity = ERROR_SEVERITY();
+        SET @ErrorState = ERROR_STATE();
+        SET @Message = ERROR_MESSAGE();
+
+        IF @transaccionIniciada = 1
+        BEGIN
+            ROLLBACK;
+        END;
+
+        IF @ErrorNumber != 50000
+        BEGIN
+            -- Si no es un error personalizado, se registra el error
+            INSERT INTO [dbo].[Errores]
+            VALUES (
+                SUSER_NAME(),
+                ERROR_NUMBER(),
+                ERROR_STATE(),
+                ERROR_SEVERITY(),
+                ERROR_LINE(),
+                ERROR_PROCEDURE(),
+                ERROR_MESSAGE(),
+                GETUTCDATE()
+            );
+        END;
+
+        RAISERROR('%s - Error Number: %i', 
+            @ErrorSeverity, @ErrorState, @Message, @ErrorNumber);
+
+    END CATCH;
+END;
---
+++ Base de datos/Procedimientos almacenados/Eventos y actividades/AsociaTEC_SP_Actividades_Lista.sql
@@ -0,0 +1,92 @@
+--------------------------------------------------------------------------
+-- Autor:       Fabián Vargas
+-- Fecha:       23-09-03
+-- Descripción: Retorna la lista de actividades de un evento
+--------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE [dbo].[AsociaTEC_SP_Actividades_Lista]
+    @IN_uuid UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON;         -- No retorna metadatos
+
+    -- CONTROL DE ERRORES
+    DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @Message VARCHAR(200);
+    DECLARE @transaccionIniciada BIT = 0;
+
+    -- DECLARACIÓN DE VARIABLES
+    DECLARE @ID_Evento INT = NULL;
+
+    BEGIN TRY
+
+        -- VALIDACIONES
+        SELECT @ID_Evento = E.[id]
+        FROM [dbo].[Eventos] E
+        WHERE E.[uuid] = @IN_uuid
+        AND E.[eliminado] = 0
+
+        IF @ID_Evento IS NULL
+        BEGIN
+            DECLARE @uuid_varchar VARCHAR(36)= (SELECT CONVERT(NVARCHAR(36), @IN_uuid))
+            RAISERROR('No existe ningún evento con el uuid %s.', 16, 1, @uuid_varchar) 
+        END
+
+        IF NOT EXISTS(
+            SELECT 1
+            FROM [dbo].[Actividades] A
+            WHERE A.[idEvento] = @ID_Evento
+            AND A.[eliminado] = 0
+        )
+        BEGIN
+            RAISERROR('No existen actividades asociadas al evento.',50000,1)
+        END
+        
+        SELECT COALESCE(
+            (SELECT 
+                A.[uuid],
+                A.[lugar],
+                A.[fechaInicio],
+                A.[fechaFin]
+            FROM [dbo].[Actividades] A
+            WHERE A.[idEvento] = @ID_Evento
+            AND A.[eliminado] = 0
+            FOR JSON PATH),
+            '[]'
+        ) as 'resultados'
+            
+        FOR JSON PATH
+
+    END TRY
+    BEGIN CATCH
+
+        SET @ErrorNumber = ERROR_NUMBER();
+        SET @ErrorSeverity = ERROR_SEVERITY();
+        SET @ErrorState = ERROR_STATE();
+        SET @Message = ERROR_MESSAGE();
+
+        IF @transaccionIniciada = 1
+        BEGIN
+            ROLLBACK;
+        END;
+
+        IF @ErrorNumber != 50000
+        BEGIN
+            -- Si no es un error personalizado, se registra el error
+            INSERT INTO [dbo].[Errores]
+            VALUES (
+                SUSER_NAME(),
+                ERROR_NUMBER(),
+                ERROR_STATE(),
+                ERROR_SEVERITY(),
+                ERROR_LINE(),
+                ERROR_PROCEDURE(),
+                ERROR_MESSAGE(),
+                GETUTCDATE()
+            );
+        END;
+
+        RAISERROR('%s - Error Number: %i', 
+            @ErrorSeverity, @ErrorState, @Message, @ErrorNumber);
+
+    END CATCH;
+END;
