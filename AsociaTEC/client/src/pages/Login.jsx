ce3d524e140b4f8270c1d4c23bcfd38c24bfc78e
Paúl Rodríguez García <70735615+psluk@users.noreply.github.com>
2023-11-06 18:02:16 -0600

    Relocated FormItems

--- AsociaTEC/client/src/pages/Login.jsx
+++ AsociaTEC/client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import FormItems from "../structures/FormItems";
+import FormItems from "../components/forms/FormItems";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useSessionContext } from "../context/SessionComponent";
