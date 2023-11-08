9f9b9f206c82cfc7f48456159a9e2ae27215b83a
fabiansj0028 <fabiansj0028@gmail.com>
2023-11-08 12:56:12 -0600

    Refactor Confirmation modal component

--- AsociaTEC/client/src/components/cards/Registration.jsx
+++ AsociaTEC/client/src/components/cards/Registration.jsx
@@ -1,33 +1,48 @@
 import React from 'react'
 import { ConfirmEventIcon, EventIcon, CancelEventIcon, QRIcon, ClockIcon, ClockWithXIcon } from '../../components/Icons';
-import { localDateTime } from '../../utils/dateFormatter';
+import { localDateTime, currentLocalHtmlAttribute } from '../../utils/dateFormatter';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { messageSettings, defaultError } from '../../utils/messageSettings';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Confirmation from '../modals/Confirmation';
+
 const Registration = ({ idEvento, carnet, nombre, inicio, fin, estado }) => {
 
     const [state, setState] = useState(estado)
+    const Navigate = useNavigate()
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal);
+    };
+
+    const compareToActualDate = (date) => {
+        const actualDate = currentLocalHtmlAttribute()
+        return actualDate > date
+    }
 
     const handleConfirm = (e) => {
-        axios.put(`/api/inscripciones/confirmar`, {evento: idEvento, carnet: carnet})
-        .then((res) => {
-            toast.success(res.data.mensaje, messageSettings);
-            setState(true)
-        }).catch((err) => {
-            toast.error(err?.response?.data?.mensaje || defaultError, messageSettings);
-        })
+        compareToActualDate(fin)
+        axios.put(`/api/inscripciones/confirmar`, { evento: idEvento, carnet: carnet })
+            .then((res) => {
+                toast.success(res.data.mensaje, messageSettings);
+                Navigate("/registrations")
+            }).catch((err) => {
+                toast.error(err?.response?.data?.mensaje || defaultError, messageSettings);
+            })
     }
 
     const handleCancel = (e) => {
-        
+
         axios.delete(`/api/inscripciones/eliminar?evento=${idEvento}&carnet=${carnet}`)
-        .then((res) => {
-            toast.success(res.data.mensaje, messageSettings);
-            window.location.reload(false);
-        }).catch((err) => {
-            toast.error(err?.response?.data?.mensaje || defaultError, messageSettings);
-        })
+            .then((res) => {
+                toast.success(res.data.mensaje, messageSettings);
+                window.location.reload(false);
+            }).catch((err) => {
+                toast.error(err?.response?.data?.mensaje || defaultError, messageSettings);
+            })
     }
 
     const handleQR = (e) => {
@@ -38,15 +53,30 @@ const Registration = ({ idEvento, carnet, nombre, inicio, fin, estado }) => {
     return (
         <div className='w-full border-2 rounded-md shadow-lg flex flex-col md:flex-row p-2 hover:bg-zinc-100 md:items-center'>
             <div className='flex flex-col grow'>
-                <p className='flex '><EventIcon className='w-6 h-6 text-venice-blue-800'/><span className='ml-2 font-bold font-serif text-venice-blue-800 text-lg'>{nombre}</span></p>
-                <p className='flex '><ClockIcon className='w-6 h-6 text-venice-blue-800'/><span className='ml-2'>{localDateTime(inicio)}</span></p>
-                <p className='flex '><ClockWithXIcon className='w-6 h-6 text-venice-blue-800'/><span className='ml-2'>{localDateTime(fin)}</span></p>
+                <p className='flex '>
+                    <EventIcon className='w-6 h-6 text-venice-blue-800' /><span className='ml-2 font-bold font-serif text-venice-blue-800 text-lg'>{nombre}</span>
+                </p>
+                <p className='flex '>
+                    <ClockIcon className='w-6 h-6 text-venice-blue-800' /><span className='ml-2'>{localDateTime(inicio)}</span>
+                </p>
+                <p className='flex '>
+                    <ClockWithXIcon className='w-6 h-6 text-venice-blue-800' /><span className='ml-2'>{localDateTime(fin)}</span>
+                </p>
             </div>
             <div className='flex md:flex-col grow-0 justify-around md:justify-between items-end'>
-                {!state &&<button onClick={handleConfirm}><ConfirmEventIcon className='w-6 h-6 text-venice-blue-800'/></button>}
-                <button onClick={handleCancel}><CancelEventIcon className='w-6 h-6 text-venice-blue-800'/></button>
-                {state && <button onClick={handleQR}><QRIcon className='w-6 h-6 text-venice-blue-800'/></button>}
+                {!state && <button onClick={handleConfirm} disabled={compareToActualDate(fin)}><ConfirmEventIcon className={`w-6 h-6 ${compareToActualDate(fin) ? 'text-gray-800' : 'text-venice-blue-800'}`} /></button>}
+                <button onClick={toggleModal} disabled={compareToActualDate(fin)}><CancelEventIcon className={`w-6 h-6 ${compareToActualDate(fin) ? 'text-gray-800' : 'text-venice-blue-800'}`} /></button>
+                {state && <button onClick={handleQR}><QRIcon className='w-6 h-6 text-venice-blue-800' /></button>}
             </div>
+            <Confirmation
+                handleClose={toggleModal}
+                handleConfirm={handleCancel}
+                title="Confirmar eliminación"
+                message="¿Está seguro de que desea eliminar esta inscripcion?"
+                confirmationText="Eliminar"
+                confirmColor="bg-red-600"
+                modal={modal}
+            />
         </div>
     )
 }
---
+++ AsociaTEC/client/src/components/modals/QRModal.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const QRModal = ({ handleClose, modal, image }) => {
+    return (
+        <>
+            {modal && (
+                <div className="w-screen h-screen top-0 left-0 right-0 bottom-0 fixed z-[100]">
+                    <div
+                        className='w-screen h-screen top-0 left-0 right-0 bottom-0 fixed z-[100] bg-gray-800 opacity-60 cursor-default'
+                        onClick={handleClose}
+                    />
+                    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white shadow-md rounded-md z-[102] w-full max-w-sm py-6 px-4 pt-8" onClick={(e) => { e.preventDefault() }}>
+                        <div className='content'>
+                            <h4 className="w-full text-center font-medium text-xl">
+                                Código QR
+                            </h4>
+                        </div>
+                        <div className="flex mt-5 justify-center gap-3">
+                            <button
+                                className="bg-transparent hover:bg-gray-300 border-2 border-gray-700 py-2 px-4 text-base rounded-md cursor-pointer text-gray-700 transition-opacity"
+                                onClick={handleClose}
+                            >
+                                Cerrar
+                            </button>
+                        </div>
+                        <button
+                            className="absolute top-2 right-2 text-slate-700 cursor-pointer"
+                            onClick={handleClose}
+                        >
+                            <FontAwesomeIcon icon={faCircleXmark} className="text-venice-blue-800" />
+                        </button>
+                    </div>
+
+                </div>
+            )}
+        </>
+    )
+}
+
+export default QRModal
