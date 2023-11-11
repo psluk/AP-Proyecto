0922f15c33b760bd009a404f0f417ca3f55f1e86
Luis Fernando <luisfmp@estudiantec.cr>
2023-11-11 14:10:45 -0600

    page editEvent (85% completed)

--- AsociaTEC/client/src/pages/Events/Edit.jsx
+++ AsociaTEC/client/src/pages/Events/Edit.jsx
@@ -4,9 +4,9 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { messageSettings, defaultError } from '../../utils/messageSettings'
-import { EventStructure } from '../../structures/Fields/CreateEventFields'
+import { EditEventStructure } from '../../structures/Fields/EditEventFields'
 import { useSessionContext } from "../../context/SessionComponent";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { isoString, currentLocalHtmlAttribute } from '../../utils/dateFormatter'
 
 /*
@@ -19,33 +19,174 @@ const titulo = req.body.titulo;
     const capacidad = req.body.capacidad;
     const categoria = req.body.categoria;
     const uuid = req.body.uuid;
+
+
+del form:
+    titulo
+    capacidad
+    lugar
+    fechaInicio
+    fechaFin
+    categoria
+    descripcion
+    especiales
+    actividades
+
 */
 
 export default function EditEvent() {
     const navigate = useNavigate();
     const [data, setData] = useState({ categoria: "" });
-    const [fields, setFields] = useState(EventStructure);
-    const {getCareerCode, getLocationCode, session } = useSessionContext();
+    const { uuid } = useParams();
+    const [fields, setFields] = useState(EditEventStructure);
+    const [event, setEvent] = useState([]);
+    const [activities, setActivities] = useState([]);
+    const { getCareerCode, getLocationCode, session } = useSessionContext();
+
+
+    const handleEditActivity = (e) => {
+        e.preventDefault()
+        // falta
+        // verificar existencia de uuid
+        // navegar a editar actividad
+
+    };
+    const handleCreateActivity = (e) => {
+        e.preventDefault()
+        // falta
+        // navegar a crear actividad
+
+
+    };
 
     useEffect(() => {
         // Redirect if logged in
         if (session.currentUser === null) {
             navigate("/");
         }
-        const careerCode = getCareerCode();
-        const locationCode = getLocationCode();
-        setData({...data, carrera: careerCode, sede: locationCode})
-        setFields((prev) => {
-            const newFields = [...prev];
-            newFields[3].min = currentLocalHtmlAttribute();
-            newFields[4].min = currentLocalHtmlAttribute();
-            return newFields;
+
+        axios.get(`/api/actividades/?uuid=${uuid}`, { withCredentials: true }).then((res) => {
+            //registro de datos de las actividades del evento
+            console.log("actividades", res.data)
+            setActivities(res.data) // guardamos las actividades
+            if (res.data.length > 0) {
+                setFields((prev) => {
+                    setData((prev) => ({
+                        ...prev,
+                        actividades: res.data[0].uuid
+                    }));
+                    const newactivities = [...prev];
+                    newactivities[8].options = res.data.map((item) => ({
+                        label: item.nombre,
+                        value: `${item.uuid}`,
+                    }));
+                    return newactivities;
+                });
+            }
+
+        }).catch((err) => {
+            toast.error(err?.response?.data?.mensaje || defaultError, messageSettings);
+            console.log("act")
         });
+
+        axios.get(`/api/eventos/detalles/?uuid=${uuid}`, { withCredentials: true }).then((res) => {
+            //registro de datos del evento
+            console.log("event", res.data)
+            setEvent(res.data)
+            if (res.data.length > 0) {
+                setFields((prev) => {
+                    setData((prev) => ({
+                        ...prev,
+                        titulo: res.data[0].titulo,
+                        capacidad: res.data[0].capacidad,
+                        lugar: res.data[0].lugar,
+                        fechaInicio: res.data[0].fechaInicio,
+                        fechaFin: res.data[0].fechaFin,
+                        categoria: res.data[0].categoria,
+                        descripcion: res.data[0].descripcion,
+                        especiales: res.data[0].especiales,
+
+                    }));
+                    const newFields = [...prev];
+                    newFields[0].placeholder = res.data[0].titulo
+                    newFields[1].placeholder = res.data[0].capacidad
+                    newFields[2].placeholder = res.data[0].lugar
+                    newFields[3].placeholder = res.data[0].fechaInicio
+                    newFields[4].placeholder = res.data[0].fechaFin
+                    newFields[5].options = res.data.map((item) => ({
+                        label: item.categoria,
+                        value: item.categoria,
+                    }));
+                    newFields[6].placeholder = res.data[0].descripcion
+                    newFields[7].placeholder = res.data[0].especiales
+                    return newFields;
+                });
+            }
+
+        }).catch((err) => {
+            toast.error(err?.response?.data?.mensaje || defaultError, messageSettings);
+            console.log("even")
+        });
+
+        axios.get("/api/eventos/categorias", { withCredentials: true }).then((res) => {
+            //registro de datos del evento
+            if (res.data.length > 0) {
+                setFields((prev) => {
+
+                    const newFields = [...prev];
+                    const aux = res.data.map((item) => ({
+                        label: item.categoria,
+                        value: item.categoria,
+                    }));
+                    const sett = new Set([...newFields[5].options.map((opt) => opt.value), ...aux.map((opt) => opt.value)])
+                    var temp = []
+
+                    sett.forEach((item) => {
+
+                        temp = [...temp, aux.find((val) => val.value === item)]
+                    });
+
+                    newFields[5].options = temp
+
+                    console.log("options", newFields[5].options)
+
+                    return newFields;
+                });
+            }
+
+        }).catch((err) => {
+            toast.error(err?.response?.data?.mensaje || defaultError, messageSettings);
+            console.log("cate")
+        });
+
+        /*
+        verificacion de loggin [ya]
+
+        solicitud a la api  (el evento) [ya]
+
+        mapeamos todos los valores a evento y data [ya]
+        llamamos a actualizar fields [ya]
+
+        on sumit, hacemos push a la api los valores
+        en logro:
+            actualizamos los valores en new evento y evento
+            llamamos a actualizar fields
+            mostramos mensaje
+        en fallo:
+            mostramos mensaje
+        */
+
+        /*
+        actualizacion de actividad: al ser en otra pagina, al regresar a esta lo de arriba se repite
+
+        */
+
     }, []);
+    
 
     const handleSubmit = (e) => {
-        console.log(data);
         e.preventDefault();
+        console.log("data", data)
 
         if (data.fechaInicio >= data.fechaFin) {
             toast.error(
@@ -55,74 +196,29 @@ export default function EditEvent() {
             return;
         };
         
-            axios.post('/api/eventos/agregar', {
-                ...data,
-                fechaInicio: isoString(data.fechaInicio),
-                fechaFin: isoString(data.fechaFin)
-            }, { withCredentials: true }).then((res) => {
-                toast.success(
-                    <p>
-                        Evento creado exitosamente
-                    </p>,
-                    messageSettings
-                );
-                navigate("/");
-            })
+        //falta configurar el axios
+        axios.post('/api/eventos/agregar', {
+            ...data,
+            fechaInicio: isoString(data.fechaInicio),
+            fechaFin: isoString(data.fechaFin)
+        }, { withCredentials: true }).then((res) => {
+            toast.success(
+                <p>
+                    Evento creado exitosamente
+                </p>,
+                messageSettings
+            );
+            navigate("/");
+        })
             .catch((err) => {
                 toast.error(
                     err?.response?.data?.mensaje || defaultError,
                     messageSettings
                 );
             });
-            toast.success("Evento creado con éxito", messageSettings);
-            navigate("/eventos");
+        toast.success("Evento creado con éxito", messageSettings);
+        navigate("/eventos");
     };
-
-    const saveCategories = (categorias) => {
-        if (categorias.length === 0) {
-            setData((prev) => ({
-                ...prev,
-                categoria: "",
-            }));
-            setFields((prev) => {
-                const newFields = [...prev];
-                newFields[5].options = [
-                    {
-                        value: "",
-                        label: "Cargando...",
-                        disabled: true,
-                    },
-                ];
-                return newFields;
-            });
-            return;
-        }
-        setFields((prev) => {
-            setData((prev) => ({
-                ...prev,
-                categoria: categorias[0].categoria,
-            }));
-            const newFields = [...prev];
-            newFields[5].options = categorias.map((categoria) => ({
-                label: categoria.categoria,
-                value: categoria.categoria,
-            }));
-            return newFields;
-        });
-    };
-
-    useEffect(() => {
-        document
-            .getElementById("categoria")
-            ?.classList.add("italic", "text-gray-400");
-
-        axios.get("/api/eventos/categorias", { withCredentials: true }).then((res) => {
-            saveCategories(res.data);
-            document
-                .getElementById("categoria")
-                ?.classList.remove("italic", "text-gray-400");
-        });
-    }, []);
 
     return (
         <div className='p-3 lg:w-[64rem] md:flex md:flex-col md:items-center w-full'>
@@ -133,19 +229,32 @@ export default function EditEvent() {
                 onSubmit={handleSubmit}
                 className="w-full max-w-4xl md:grid md:grid-cols-2 md:gap-10 md:mt-4 p-6 space-y-4 md:space-y-0">
                 <FormItems
-                    fields={EditEventStructure}
+                    fields={fields}
                     formItemsData={data}
                     setFormItemsData={setData}
                 />
-                <div className='col-span-2 flex flex-col items-center'>
+                <div className=' flex space-x-4'>
                     <button
-                        className="bg-venice-blue-700 text-white py-2 px-4 rounded-lg w-fit"
-                        type="submit"
+                        className="bg-venice-blue-700 hover:bg-venice-blue-800 text-white py-2 px-4 rounded-lg w-fit"
+                        onClick={handleEditActivity}
                     >
-                        Crear evento
+                        Editar la actividad
+                    </button>
+                    <button
+                        className="bg-venice-blue-700 hover:bg-venice-blue-800 text-white py-2 px-4 rounded-lg w-fit"
+                        onClick={handleCreateActivity}
+                    >
+                        Agregar actividad
                     </button>
                 </div>
             </form>
+            <button
+                className="bg-venice-blue-700 hover:bg-venice-blue-800 text-white py-2 px-4 rounded-lg w-fit"
+                type="submit"
+            >
+                Editar evento
+            </button>
+
             <p className="text-center">
                 <a
                     className="text-venice-blue-700 hover:underline cursor-pointer"
---
+++ AsociaTEC/client/src/structures/Fields/EditEventFields.js
@@ -0,0 +1,71 @@
+export const EditEventStructure = [
+    {
+        label: "Título del evento",
+        type: "text",
+        name: "titulo",
+        placeholder: "",
+        required: false
+    },
+    {
+        label: "Capacidad",
+        type: "number",
+        name: "capacidad",
+        placeholder: "Capacidad del evento",
+        required: false,
+    },
+    {
+        label: "Lugar",
+        type: "text",
+        name: "lugar",
+        placeholder: "Lugar del evento",
+        required: false,
+    },
+    {
+        label: "Fecha de inicio",
+        type: "datetime-local",
+        name: "fechaInicio",
+        placeholder: "Fecha de Inicio",
+        required: false,
+    },
+    {
+        label: "Fecha de finalización",
+        type: "datetime-local",
+        name: "fechaFin",
+        placeholder: "Fecha de Finalización",
+        required: false,
+    },
+    {
+        label: "Categoría",
+        type: "select",
+        name: "categoria",
+        options: [
+            { value: "", label: "Cargando...", disabled: true },
+        ],
+        required: false,
+    },
+    {
+        label: "Descripción",
+        type: "textarea",
+        name: "descripcion",
+        placeholder: "Descripción del evento",
+        rows: 5,
+        required: false,
+    },
+    {
+        label: "Recursos especiales",
+        type: "textarea",
+        name: "especiales",
+        placeholder: "Recursos especiales",
+        rows: 5,
+        required: false,
+    },
+    {
+        label: "actividades",
+        type: "select",
+        name: "actividades",
+        options: [
+            { value: "", label: "Cargando...", disabled: true },
+        ],
+        required: false,
+    }
+]
