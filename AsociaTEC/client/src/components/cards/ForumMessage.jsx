8045b04bff36e5a388d1fcf73f49ae23cce16245
Luis Fernando <luisfmp@estudiantec.cr>
2023-11-12 11:55:43 -0600

    Page (message) of conversations of Forum

--- AsociaTEC/client/src/components/cards/ForumMessage.jsx
+++ AsociaTEC/client/src/components/cards/ForumMessage.jsx
@@ -1,70 +1,52 @@
-import { faGraduationCap,faBookBookmark, faCircleCheck, faXmarkCircle} from "@fortawesome/free-solid-svg-icons";
+import { faGraduationCap, faBookBookmark, faCircleCheck, faXmarkCircle, faPerson, faMessage } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate, useParams } from "react-router-dom";
 import Confirmation from "../modals/Confirmation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const ForumMessage = ({proposal, onDecline, onAccept }) => {
+const ForumMessage = ({ message }) => {
     const navigate = useNavigate();
-    const { uuid } = useParams();
-    const [modal, setModal] = useState(false);
+    const [asociation, setAsociation] = useState(false);
 
-    const toggleModal = () => {
-        setModal(!modal);
-    };
-    
-    const goToDetail = (e) => {
-        e.preventDefault();
-        navigate(details);
-    };
-    const acceptProposal = (e) => {
-        e.preventDefault();
-        onAccept(proposal.id);
-    };
-    
-    const declineProposal = () => {
-        setModal(false);
-        onDecline(proposal.id);
-    };
+    // Load locations and association data
+    useEffect(() => {
+
+        if (message.autor.carnet === undefined) {
+            //tenemos a una asociacion
+            setAsociation(true)
+        }
+
+    }, []);
+
+
 
     return (
         <div className="md:shadow-md border-b md:border-none md:m-2 md:rounded-xl p-3 flex flex-col md:flex-row md:items-center">
             <div className="grow">
-                <a
-                    href={details}
-                    onClick={goToDetail}>
-                        <p
-                            className="font-bold font-serif text-venice-blue-800 text-lg">
-                                {proposal.titulo}
-                        </p>
-                </a>
+
                 <div className="flex flex-row items-center">
-                    <FontAwesomeIcon icon={faBookBookmark} className="mr-2 text-venice-blue-800" />
-                    <p><strong>Temática: </strong> {proposal.tematica}</p>
+                    <FontAwesomeIcon icon={faPerson} className="mr-2 text-venice-blue-800" />
+                    <p><strong>Autor: </strong> {message.autor.nombre}</p>
                 </div>
+                {
+                    asociation ?
+                        <div className="flex flex-row items-center">
+                            <FontAwesomeIcon icon={faGraduationCap} className="mr-2 text-venice-blue-800" />
+                            <p><strong>Asociacion</strong></p>
+                        </div> :
+                        <div className="flex flex-row items-center">
+                            <FontAwesomeIcon icon={faGraduationCap} className="mr-2 text-venice-blue-800" />
+                            <p><strong>Carnet: </strong>  {message.autor.carnet}</p>
+                        </div>
+                }
                 <div className="flex flex-row items-center">
-                    <FontAwesomeIcon icon={faGraduationCap} className="mr-2 text-venice-blue-800" />
-                    <p><strong>Objetivos: </strong>  {proposal.objetivos}</p>
-                </div>
+                            <FontAwesomeIcon icon={faMessage} className="mr-2 text-venice-blue-800" />
+                            <p><strong>Mensaje: </strong>  {message.contenido}</p>
+                        </div>
             </div>
-            <div className="flex flex-row mt-2 md:mt-0 md:ml-2 md:flex-col grow-0 justify-around md:justify-center md:space-y-2">
-                <button
-                    onClick={acceptProposal}>
-                        <FontAwesomeIcon className="text-xl text-venice-blue-800" icon={faCircleCheck} title="Modificar" />
-                </button>
-                <FontAwesomeIcon className="text-xl text-red-700 cursor-pointer" icon={faXmarkCircle} onClick={toggleModal} title="Eliminar" />
-            </div>
-            <Confirmation
-                handleClose={toggleModal}
-                handleConfirm={declineProposal}
-                title="Confirmar eliminación"
-                message="¿Está seguro de que desea rechazar la propuesta?"
-                confirmationText="Eliminar"
-                confirmColor="bg-red-600"
-                modal={modal}
-            />
         </div>
     )
 };
 
 export default ForumMessage;
+
--- AsociaTEC/client/src/pages/Forum/ForumMessages.jsx
+++ AsociaTEC/client/src/pages/Forum/ForumMessages.jsx
@@ -5,36 +5,37 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { messageSettings, defaultError } from "../../utils/messageSettings";
 import { useSessionContext } from "../../context/SessionComponent";
-import Forum from "../../components/cards/Forum";
+import ForumMessage from "../../components/cards/ForumMessage";
 
 const ForumMessages = () => {
     const navigate = useNavigate();
-    const { getUserType, isLoggedIn, getUniId } = useSessionContext();
+    const { uuid } = useParams();
+    const [modal, setModal] = useState(false);
+    const { getUserType, isLoggedIn, getEmail } = useSessionContext();
     const [proposal, setProposal] = useState([]);
-    const [forum, setForum] = useState([]);
+    const [messages, setMessages] = useState([]);
     const [data, setData] = useState({});
 
-    const onClick = (e, uuid) => {
-        e.preventDefault();
-        navigate(`/forum/conversation/${uuid}`)
-    }
-
-    const handleSearch = (e) => {
-        e.preventDefault();
-        console.log(data.titulo)
-        axios.get(`/api/conversaciones?titulo=${data.titulo}`)
-            .then((response) => {
-                const prop = response.data
-                setForum(prop)
-            })
-            .catch((error) => {
-                toast.error(error?.response?.data?.mensaje || "No se logró cargar las conversaciones", messageSettings);
-            });
-    }
+    const toggleModal = () => {
+        setModal(!modal);
+    };
 
     const handleCreate = (e) => {
         e.preventDefault();
-        
+
+        axios.post(`/api/conversaciones/mensajes/agregar`, {
+            uuid : uuid,
+            contenido : data.contenido,
+            correo : getEmail()
+            }, { withCredentials: true })
+        .then((response) => {
+
+            toast.success("Mensaje publicado correctamente", messageSettings);
+            toggleModal()
+        })
+        .catch((error) => {
+            toast.error(error?.response?.data?.mensaje || "No se logró cargar los mensajes", messageSettings);
+        });
     }
 
 
@@ -46,26 +47,31 @@ const ForumMessages = () => {
             toast.error("Sesión no iniciada", messageSettings);
         }
 
-        axios.get(`/api/conversaciones/`)
+        axios.get(`/api/conversaciones/mensajes?uuid=${uuid}`)
             .then((response) => {
                 const prop = response.data
                 console.log(prop)
-                setForum(prop)
+                setMessages(prop)
             })
             .catch((error) => {
-                toast.error(error?.response?.data?.mensaje || "No se logró cargar las conversaciones", messageSettings);
+                toast.error(error?.response?.data?.mensaje || "No se logró cargar los mensajes", messageSettings);
             });
 
     }, []);
 
+    useEffect(() => {
 
+        axios.get(`/api/conversaciones/mensajes?uuid=${uuid}`)
+            .then((response) => {
+                const prop = response.data
+                console.log(prop)
+                setMessages(prop)
+            })
+            .catch((error) => {
+                toast.error(error?.response?.data?.mensaje || "No se logró cargar los mensajes", messageSettings);
+            });
 
-
-
-    //barra buscadora -> form input
-    // seccion medio donde se lista
-
-    // hacer card para mostrar info
+    }, [modal]);
 
     //button (tarjetas de foro)
     // [ tarjetas de chat]
@@ -79,15 +85,24 @@ const ForumMessages = () => {
             <h1 className="text-center text-4xl font-serif text-venice-blue-800 font-bold">
                 Foro
             </h1>
-            <form className="space-y-4 flex flex-col items-center w-full" onSubmit={handleSearch}>
+            {
+                messages.length > 0
+                    ? <div className="">{
+                        messages.map((item, index) => (
+                            <ForumMessage message={item} key={index}/>
+                        ))
+                    }</div>
+                    : <p className="text-gray-600 italic text-center">Aun no hay mensajes</p>
+            }
+            <form className="space-y-4 flex flex-col items-center w-full" onSubmit={handleCreate}>
                 <FormItems
                     fields={[{
-                        label: "Título",
-                        type: "text",
-                        name: "titulo",
-                        placeholder: "Título de la conversación",
-                        required: false,
-                        maxLength: 64,
+                        label: "Contenido",
+                        type: "textarea",
+                        name: "contenido",
+                        placeholder: "Ingrese el texto",
+                        required: true,
+                        rows: 5
                     }]}
                     formItemsData={data}
                     setFormItemsData={setData}
@@ -97,26 +112,17 @@ const ForumMessages = () => {
                     type="submit"
                     key={"submit"}
                 >
-                    Buscar
-                </button>
-                <button
-                    className=" bg-venice-blue-500 text-white py-2 px-4 rounded-lg w-fit"
-                    onClick={handleCreate}>
-                    Crear nueva Conversacion
+                    Enviar mensaje
                 </button>
             </form>
-            {
-                forum.length > 0
-                    ? <div className="">{
-                        forum.map((item, index) => (
-                            <Forum forum={item} key={index} click={onClick} />
-                        ))
-                    }</div>
-                    : <p className="text-gray-600 italic text-center">Cargando...</p>
-            }
-
         </div>
     );
 };
 
 export default ForumMessages;
+
+/**
+
+
+
+ */
