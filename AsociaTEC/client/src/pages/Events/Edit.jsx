c966fd95d4d81104a8f159a57aaa8252d662ef51
Luis Fernando <luisfmp@estudiantec.cr>
2023-11-12 19:36:20 -0600

    page edit event (fixed)

--- AsociaTEC/client/src/pages/Events/Edit.jsx
+++ AsociaTEC/client/src/pages/Events/Edit.jsx
@@ -16,13 +16,53 @@ export default function EditEvent() {
     const [fields, setFields] = useState(EditEventStructure);
     const [event, setEvent] = useState([]);
     const [cambio, setCambio] = useState(false);
-    const [activities, setActivities] = useState([]);
-    const {session } = useSessionContext();
+    const { session } = useSessionContext();
 
 
     const handleActivity = (e) => {
         e.preventDefault()
         navigate(`/event/activities/${uuid}`)
+    };
+
+    const handleCollaborator = (e) => {
+        e.preventDefault()
+        navigate(`/collaborators/${uuid}`)
+    };
+
+    const followUP = () => {
+        axios.get("/api/eventos/categorias", { withCredentials: true }).then((res) => {
+            //registro de datos del evento
+            setTimeout(() => {
+                if (res.data.length > 0) {
+                    setFields((prev) => {
+
+                        const newFields = [...prev];
+                        const aux = res.data.map((item) => ({
+                            label: item.categoria,
+                            value: item.categoria,
+                        }));
+                        const sett = new Set([...newFields[5].options.map((opt) => opt.value), ...aux.map((opt) => opt.value)])
+                        var temp = []
+
+                        sett.forEach((item) => {
+
+                            temp = [...temp, aux.find((val) => val.value === item)]
+                        });
+
+                        newFields[5].options = temp
+
+                        console.log("options", newFields[5].options)
+
+                        return newFields;
+                    });
+                }
+                setCambio(true)
+            }, 650);
+
+        }).catch((err) => {
+            toast.error(err?.response?.data?.mensaje || defaultError, messageSettings);
+            console.log("cate")
+        });
     };
 
     useEffect(() => {
@@ -66,68 +106,13 @@ export default function EditEvent() {
                 });
             }
             setCambio(true)
+            followUP()
 
         }).catch((err) => {
             toast.error(err?.response?.data?.mensaje || defaultError, messageSettings);
             console.log("even")
         });
-
-        axios.get("/api/eventos/categorias", { withCredentials: true }).then((res) => {
-            //registro de datos del evento
-            setTimeout(() => {
-                if (res.data.length > 0) {
-                    setFields((prev) => {
-    
-                        const newFields = [...prev];
-                        const aux = res.data.map((item) => ({
-                            label: item.categoria,
-                            value: item.categoria,
-                        }));
-                        const sett = new Set([...newFields[5].options.map((opt) => opt.value), ...aux.map((opt) => opt.value)])
-                        var temp = []
-    
-                        sett.forEach((item) => {
-    
-                            temp = [...temp, aux.find((val) => val.value === item)]
-                        });
-    
-                        newFields[5].options = temp
-    
-                        console.log("options", newFields[5].options)
-    
-                        return newFields;
-                    });
-                }
-                setCambio(true)
-              }, 500);
-
-        }).catch((err) => {
-            toast.error(err?.response?.data?.mensaje || defaultError, messageSettings);
-            console.log("cate")
-        });
         setCambio(true)
-
-        /*
-        verificacion de loggin [ya]
-
-        solicitud a la api  (el evento) [ya]
-
-        mapeamos todos los valores a evento y data [ya]
-        llamamos a actualizar fields [ya]
-
-        on sumit, hacemos push a la api los valores
-        en logro:
-            actualizamos los valores en new evento y evento
-            llamamos a actualizar fields
-            mostramos mensaje
-        en fallo:
-            mostramos mensaje
-        */
-
-        /*
-        actualizacion de actividad: al ser en otra pagina, al regresar a esta lo de arriba se repite
-
-        */
 
     }, []);
 
@@ -188,7 +173,7 @@ export default function EditEvent() {
             uuid: uuid
         }
 
-        console.log("hhh",x)
+        console.log("hhh", x)
         /*
         titulo = req.body.titulo;
         descripcion = req.body.descripcion;
@@ -220,30 +205,43 @@ export default function EditEvent() {
             <h1 className="text-center text-4xl font-serif text-venice-blue-800 font-bold my-4">
                 Editar Evento
             </h1>
-
-            <form
-                onSubmit={handleSubmit}
-                className="w-full max-w-4xl md:grid md:grid-cols-2 md:gap-10 md:mt-4 p-6 space-y-4 md:space-y-0">
-                <FormItems
-                    fields={fields}
-                    formItemsData={data}
-                    setFormItemsData={setData}
-                />
-                <div className=' flex space-x-4'>
+            <div className= "w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-10 p-6 ">
                     <button
                         className="bg-venice-blue-700 hover:bg-venice-blue-800 text-white py-2 px-4 rounded-lg w-fit"
                         onClick={handleActivity}
                     >
-                        ver actividades
+                        Ver actividades
+                    </button>
+
+
+                    <button
+                        className="bg-venice-blue-700 hover:bg-venice-blue-800 text-white py-2 px-4 rounded-lg w-fit"
+                        onClick={handleCollaborator}
+                    >
+                        Ver colaboradores
+                    </button>
+            </div>
+            <form
+                onSubmit={handleSubmit}
+                className="w-full max-w-4xl md:mt-4 p-6 ">
+                <div className='md:grid md:grid-cols-2 md:gap-10 space-y-4 md:space-y-0'>
+                    <FormItems
+                        fields={fields}
+                        formItemsData={data}
+                        setFormItemsData={setData}
+                    />
+                </div>
+                <div className='w-full flex justify-center mt-12'>
+                    <button
+                        className="bg-venice-blue-700 hover:bg-venice-blue-800 text-white py-2 px-4 rounded-lg w-fit bg-center"
+                        type="submit"
+                        key="submit"
+                    >
+                        Editar evento
                     </button>
                 </div>
-                <button
-                    className="bg-venice-blue-700 hover:bg-venice-blue-800 text-white py-2 px-4 rounded-lg w-fit bg-center"
-                    type="submit"
-                >
-                    Editar evento
-                </button>
             </form>
+
             <p className="text-center">
                 <a
                     className="text-venice-blue-700 hover:underline cursor-pointer"
